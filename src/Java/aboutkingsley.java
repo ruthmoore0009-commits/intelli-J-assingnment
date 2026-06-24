2873e8ab7cf4e2e5f0fb72659e4c1fb1bc48564c
kingsley <ruthmoore0009@gmail.com>
2026-06-24 13:20:11 +0000

    Create aboutkingsley.java

---
+++ src/Java/aboutkingsley.java
@@ -0,0 +1,31 @@
+package Java;
+
+public class aboutkingsley {
+    static void main(String[] args) {
+        // Personal details
+        String name = "Kingsley Malir";
+        int age = 20; //Not actual age
+        String school = "Koforidua Technical University";
+        double height = 1.40;
+        String hobby = "Football";
+        String Nationality = ("Ghanaian");
+
+
+        //Student (boolean)
+        boolean isStudent = true;
+
+        //displaying information
+        System.out.println("------About My Self------");
+        System.out.println("Name:" + name);
+        System.out.println("age:" + age);
+        System.out.println("School:" + school);
+        System.out.println("Height:" + height);
+        System.out.println("Hobby:" + hobby);
+        System.out.println("Student:" + isStudent);
+        System.out.println("Nationality:" + Nationality);
+
+        //
+
+
+
+    }}
