7d5b970ce827ba61644d4b28bd0f848bd2e719bf
kingsley <ruthmoore0009@gmail.com>
2026-06-24 13:39:57 +0000

    Update aboutkingsley.java

--- src/Java/aboutkingsley.java
+++ src/Java/aboutkingsley.java
@@ -5,27 +5,36 @@ public class aboutkingsley {
         // Personal details
         String name = "Kingsley Malir";
         int age = 20; //Not actual age
+        int DOB = 12/12/2000;
         String school = "Koforidua Technical University";
         double height = 1.40;
         String hobby = "Football";
         String Nationality = ("Ghanaian");
 
 
+
         //Student (boolean)
         boolean isStudent = true;
 
         //displaying information
+        System.out.println("=========================");
         System.out.println("------About My Self------");
+        System.out.println("=========================");
         System.out.println("Name:" + name);
         System.out.println("age:" + age);
+        System.out.println("DOB:" + DOB);
         System.out.println("School:" + school);
         System.out.println("Height:" + height);
         System.out.println("Hobby:" + hobby);
         System.out.println("Student:" + isStudent);
         System.out.println("Nationality:" + Nationality);
 
-        //
 
+
+
+        //Message
+        System.out.println("\n Will like to be a programmer. hope my little coding " +
+                "foundation will soar high");
 
 
     }}
